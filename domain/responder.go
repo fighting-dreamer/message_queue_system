e6ee0514fb2e8687469ed3c0870a6e7c886e5675
nipun.jindal <mansapatiala@gmail.com>
2021-06-26 21:21:34 +0530

    adds respnse and err response for api handlers

---
+++ domain/responder.go
@@ -0,0 +1,41 @@
+package domain
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"nipun.io/message_queue/appcontext"
+)
+
+type errorResponse struct {
+	Errors []string `json:"errors"`
+}
+
+func WriteResponse(status int, response interface{}, rw http.ResponseWriter) {
+	if response == nil {
+		response = struct{}{}
+	}
+	body, err := json.Marshal(response)
+	if err != nil {
+		appcontext.Logger.Error().Err(err)
+		status = http.StatusInternalServerError
+	}
+	rw.Header().Add("Content-Type", "application/json")
+	rw.WriteHeader(status)
+	rw.Write(body)
+}
+
+func WriteErrorResponse(status int, errorMessages []string, rw http.ResponseWriter) {
+	if errorMessages == nil {
+		errorMessages = []string{}
+	}
+	errorResponse := errorResponse{Errors: errorMessages}
+	respBytes, err := json.Marshal(errorResponse)
+	if err != nil {
+		appcontext.Logger.Error().Err(err)
+		status = http.StatusInternalServerError
+	}
+	rw.Header().Add("Content-Type", "application/json")
+	rw.WriteHeader(status)
+	rw.Write(respBytes)
+}
